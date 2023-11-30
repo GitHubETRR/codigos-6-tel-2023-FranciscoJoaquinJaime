345382a9095d950eed78e9e04722487952f34db0
Fran Jaime <112029207+FranciscoJoaquinJaime@users.noreply.github.com>
2023-11-30 08:57:05 -0300

    englishverbs.py

--- englishverbs.py
+++ englishverbs.py
@@ -2,15 +2,14 @@ def verify (verb:str):
   past_simple = ['ed', 'd', 'ied']
   past_perfect = ['had', 'have']
 
-  if any(verb.endswith(verb_end) for verb_end in past_simple):
+  if any(verb.endswith(i) for i in past_simple):
       return "Past Simple"
   elif verb.endswith('ing'):
       return "Present Continous"
-  elif any(verb.startswith(verb_start) for verb_start in past_perfect):
+  elif any(verb.startswith(i) for i in past_perfect):
       return "Past Perfect"
   else:
       return None
-      sys.exit()
 
 user_verb = input("Ingrese un verbo en inglés: ")
 
@@ -18,7 +17,6 @@ conjugation = verify(user_verb)
 
 if conjugation is None:
     print ("No es un verbo registrado")
-    
+
 else:
     print("El verbo", user_verb, "está conjugado en", conjugation)
-
